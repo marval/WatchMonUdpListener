206fcab54d088206651d029967700f974ca76d94
Martin Valov <martin.valov@gmail.com>
2019-09-11 15:52:49 +0200

    Updating with latest Batrium FW

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ var fs = require('fs');
 
 //Loading configuration file. 
 try {
-	var config = JSON.parse(fs.readFileSync('config.json', "utf8"));
+  var config = JSON.parse(fs.readFileSync('config.json','utf8'));
 }
 catch (e) {
-	errorText('Could not load configuration file. Will therefore not send any data out. file missing is config.json. Perhaps copy the dist file?'); 
-	console.log(e);
-	var config = {'all':{'mqtt':{},'influx':{}}, 'hej': {}};
+  errorText('Could not load configuration file. Will therefore not send any data out. file missing is config.json. Perhaps copy the dist file?'); 
+  console.log(e);
+  var config = {'all':{'mqtt':{},'influx':{}}, 'hej': {}};
 }
 
 //MQTT server  generally localhost
@@ -35,10 +35,10 @@ var influxdatabase = (config.config.influxdatabase) ? config.config.influxdataba
 
 //Setup MQTT
 options={
-	clientId:"raspi",
-	username:mqttusername,
-	password:mqttpassword,
-	clean:true};
+  clientId:"raspi",
+  username:mqttusername,
+  password:mqttpassword,
+  clean:true};
 
 var client  = mqtt.connect('mqtt://' + mqtthost, options)
 
@@ -52,22 +52,21 @@ const influx = new Influx.InfluxDB({
 // input data object
 // output payload data in json form
 function getPayload(data) {
-     var payload = new Parser()
-         .string('first', { encoding: 'ascii', length: 1 })
-         .int16le('MessageId', { formatter: (x) => {return x.toString(16);}})
-         .string('nd', { encoding: 'ascii', length: 1 })
-         .int16le('SystemId')
-         .int16le('hubId')
-	
-	 return payload.parse(data);
+  var payload = new Parser()
+      .string('first', { encoding: 'ascii', length: 1 })
+      .int16le('MessageId', { formatter: (x) => {return x.toString(16);}})
+      .string('nd', { encoding: 'ascii', length: 1 })
+      .int16le('SystemId')
+      .int16le('hubId')
+
+  return payload.parse(data);
 }
 
 function getArrayObject(msg) {
 
-	obj = Object.assign(payload, eval(messages[payload.MessageId])(msg))
-	
-		
-	return obj;
+  obj = Object.assign(payload, eval(messages[payload.MessageId])(msg))
+
+  return obj;
 }
 
 
@@ -78,40 +77,40 @@ function getArrayObject(msg) {
 // It takes SystemId, MessageID and the data to send out. The data must be in Json format
 function sendMqtt(SystemId,MessageId,data) {
 
-	client.publish('Batrium/' + SystemId + '/' + MessageId , JSON.stringify(data));		
-	if (debugMQTT) console.log('Data sent to MQTT: Batrium/' + SystemId + '/' + MessageId);
+  client.publish('Batrium/' + SystemId + '/' + MessageId , JSON.stringify(data));		
+  if (debugMQTT) console.log('Data sent to MQTT: Batrium/' + SystemId + '/' + MessageId);
 }
 
 
 // Function to send data to influx database
 // Input is the data in Json format and a tag to use in influx
 function sendInflux(data, tag) {
-	// In config you can set the extra tags and or serie to use. If not set they all go to generic
-	tg = { systemId: data.SystemId, messageId: data.MessageId, messageType: (config[data.MessageId] && config[data.MessageId].tag  ) ? config[data.MessageId].tag: 'generic' };
-	// IF its node based we need to add the node-tag to it as well
-	(config[data.MessageId] && config[data.MessageId].tagID  ) ? tg['nodeID'] =  data[config[data.MessageId].tagID]  : '';
-	
-	influx.writeMeasurement((config[data.MessageId] && config[data.MessageId].serie  ) ? config[data.MessageId].serie: 'generic', [
-  	{
-	  tags: tg,
-	  fields: data,
-  	}
-	], {
-		precision: 's'
-	}
+  // In config you can set the extra tags and or serie to use. If not set they all go to generic
+  tg = { systemId: data.SystemId, messageId: data.MessageId, messageType: (config[data.MessageId] && config[data.MessageId].tag  ) ? config[data.MessageId].tag: 'generic' };
+  // IF its node based we need to add the node-tag to it as well
+  (config[data.MessageId] && config[data.MessageId].tagID  ) ? tg['nodeID'] =  data[config[data.MessageId].tagID]  : '';
 
-	
-	);
+  influx.writeMeasurement((config[data.MessageId] && config[data.MessageId].serie  ) ? config[data.MessageId].serie: 'generic', [
+    {
+    tags: tg,
+    fields: data,
+    }
+  ], {
+    precision: 's'
+  }
+
+
+  );
 };
 
 function errorText(string) {
-	console.log('\x1b[31m%s\x1b[0m', string);
+  console.log('\x1b[31m%s\x1b[0m', string);
 }
 function infoText(string) {
-	console.log('\x1b[32m%s\x1b[0m', string);
+  console.log('\x1b[32m%s\x1b[0m', string);
 }
 function runText(string) {
-	console.log('\x1b[34m%s\x1b[0m', string);
+  console.log('\x1b[34m%s\x1b[0m', string);
 }
 
 
@@ -132,17 +131,17 @@ console.log('Batrium logger started');
 // Function to load in all parsers from the payload folder. Those not able to load will be discarded during startup. 
 var messages = {};
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
-	try {
+  try {
 
-		require("./payload/" + file)();
-		load = file.split("_")[1];
-		messages[load.toLowerCase()] = 'parse_' + load.toLowerCase();
-		infoText('Loaded file: ' + file);
+    require("./payload/" + file)();
+    load = file.split("_")[1];
+    messages[load.toLowerCase()] = 'parse_' + load.toLowerCase();
+    infoText('Loaded file: ' + file);
 
-	}
-	catch (e) {
- 		errorText('Could not load file: ' + file)
-	}
+  }
+  catch (e) {
+     errorText('Could not load file: ' + file)
+  }
 });
 
 
@@ -153,29 +152,30 @@ debugMQTT = false;
 var tag;
 // Parse new messages incomming from Batrium 
 server.on('message',function(msg,info){
-	payload = getPayload(msg);
-	//process.stdout.write("\x1b[34mData recieved from\x1b[0m " + payload.SystemId  + " \x1b[34mand message is:\x1b[0m " + payload.MessageId + " \r");
-	messageID = payload.MessageId.substring(0,2);	// uggly but it just brings out the message id and removes the versiion
-	if(payload.MessageId in messages) {
-		// If error in message lets try/catch it so we dont rage quit
-		try {
-			obj = Object.assign(payload, eval(messages[payload.MessageId])(msg)); 
-			if (debug) console.log(obj);	
-			// check if the message id is present in the config. This dont care what version is there if file exist
-			if (config[messageID] && config[messageID].mqtt || config.all.mqtt) sendMqtt(payload.SystemId,payload.MessageId,obj);
-			if (config[messageID] && config[messageID].influx || config.all.influx) sendInflux(obj, tag);
-		 	// Below is used if you use messageid and version in the configuration file	
-			if (config[payload.MessageId] && config[payload.MessageId].mqtt || config.all.mqtt) sendMqtt(payload.SystemId,payload.MessageId,obj);
-			if (config[payload.MessageId] && config[payload.MessageId].influx || config.all.influx) sendInflux(obj, tag);
-		} catch (e) {
-			errorText('Couldnt get payload for ' + payload.MessageId + ' Size: %s',msg.length);
-			console.log(e);
-			//process.exit(1);  //Lets end the program if we find errors processing the data. for error searching
-		}
+  payload = getPayload(msg);
+  
+  //process.stdout.write("\x1b[34mData recieved from\x1b[0m " + payload.SystemId  + " \x1b[34mand message is:\x1b[0m " + payload.MessageId + " \r");
+  messageID = payload.MessageId.substring(0, 2);	// uggly but it just brings out the message id and removes the versiion
+  if(payload.MessageId in messages) {
+    // If error in message lets try/catch it so we dont rage quit
+    try {
+      obj = Object.assign(payload, eval(messages[payload.MessageId])(msg)); 
+      if (debug) console.log(obj);	
+      // check if the message id is present in the config. This dont care what version is there if file exist
+      if (config[messageID] && config[messageID].mqtt || config.all.mqtt) sendMqtt(payload.SystemId,payload.MessageId,obj);
+      if (config[messageID] && config[messageID].influx || config.all.influx) sendInflux(obj, tag);
+       // Below is used if you use messageid and version in the configuration file	
+      if (config[payload.MessageId] && config[payload.MessageId].mqtt || config.all.mqtt) sendMqtt(payload.SystemId,payload.MessageId,obj);
+      if (config[payload.MessageId] && config[payload.MessageId].influx || config.all.influx) sendInflux(obj, tag);
+    } catch (e) {
+      errorText('Couldnt get payload for ' + payload.MessageId + ' Size: %s',msg.length);
+      console.log(e);
+      //process.exit(1);  //Lets end the program if we find errors processing the data. for error searching
+    }
 
-	} else {
-		//console.log('Message with message id %s does not exist', payload.MessageId);
-	}
+  } else {
+    //console.log('Message with message id %s does not exist', payload.MessageId);
+  }
 
 });
 
@@ -196,8 +196,8 @@ server.on('close',function(){
 
 // Extra debugging added
 process.on('unhandledRejection', (reason, p) => {
-	  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
-	    // application specific logging, throwing an error, or other logic here
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+      // application specific logging, throwing an error, or other logic here
 });
 
 server.bind(18542);
